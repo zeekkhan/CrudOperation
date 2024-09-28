99465095d30ac3414a841f12781a27f0febc7ffd
zeekkhan <zeekkhanz10000@gmail.com>
2024-09-28 22:34:03 +0500

    materail ui installed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
 import {
   Box, Button, Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Paper,
   TextField, Dialog, DialogActions, DialogContent, DialogTitle
